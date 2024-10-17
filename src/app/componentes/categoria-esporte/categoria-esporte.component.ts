235ed2ea408382a543cba98991c7c6818aa26b7d
Nathalia-Valentim <nathadelino@hotmail.com>
2024-10-16 22:30:02 -0300

    footer

--- src/app/componentes/categoria-esporte/categoria-esporte.component.ts
+++ src/app/componentes/categoria-esporte/categoria-esporte.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-categoria-esporte',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './categoria-esporte.component.html',
   styleUrl: './categoria-esporte.component.css'
 })
-export class CategoriaEsporteComponent {
 
+export class CategoriaEsporteComponent {
+  categorias = [
+    { nome: 'Futebol', imagem: 'images/futebol.png' },
+    { nome: 'Basquete', imagem: 'images/basquete.png' },
+    { nome: 'Ciclismo', imagem: 'images/ciclismo.png' },
+    { nome: 'Natação', imagem: 'images/natacao.png' },
+    { nome: 'Dança', imagem: 'images/danca.png' },
+    { nome: 'Corrida', imagem: 'images/corrida.png' },
+    { nome: 'Vôlei', imagem: 'images/volei.png' },
+    { nome: 'Surf', imagem: 'images/surf.png' },
+  ];
 }
